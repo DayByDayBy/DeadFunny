5849f928b5c9e2829ebd59a6d314b4270d1527f7
YAP! <gabriele.maddaloni@gmail.com>
2024-01-26 22:12:54 +0000

    Added sound detection.

---
+++ Assets/_Code/SoundDetector.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDetector : MonoBehaviour
+{
+
+    #region member variables
+
+    public float _sensitivity;
+    public float _vol = 0;
+    public float _maxVolume = 0;
+    public float _offset;
+    public float[] _samples;
+
+    private AudioSource _audio;
+
+    #endregion
+
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+
+        _audio.clip = Microphone.Start(Microphone.devices[0], true, 180, 44100);
+
+        _audio.Play();
+
+        _samples = new float[4096]; // 4096 = around 85 ms of samples
+    }
+
+    void Update()
+    {
+        _vol = GetRMS(0) + GetRMS(1);
+        _vol *= _sensitivity - _offset;
+    }
+
+    float GetRMS(int channel)
+    {
+        _audio.GetOutputData(_samples, channel); // fill array with samples
+        float sum = 0;
+        for (var i = 0; i < 4096; i++)
+        {
+            sum += _samples[i] * _samples[i]; // sum squared samples
+        }
+        return Mathf.Sqrt(sum / 4096); // rms = square root of average 
+    }
+
+    public float GetVolume()
+    {
+        return _vol;
+    }
+
+    public void SetMaxVolume()
+    {
+        _maxVolume = _vol;
+    }
+
+}
